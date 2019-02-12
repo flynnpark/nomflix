18cd7cc42b4f3ef97bb4e351a8767713bded5d37
Flynn <wphestiraid@gmail.com>
2019-02-12 18:45:36 +0900

    API 호출 추가

--- src/api.ts
+++ src/api.ts
@@ -12,22 +12,50 @@ interface IMoviesAPI {
   nowPlaying: () => AxiosPromise<any>;
   upcoming: () => AxiosPromise<any>;
   popular: () => AxiosPromise<any>;
+  movieDetail: (id: number) => AxiosPromise<any>;
+  search: (term: string) => AxiosPromise<any>;
 }
 
 export const moviesAPI: IMoviesAPI = {
   nowPlaying: () => API.get('movie/now_playing'),
   upcoming: () => API.get('movie/upcoming'),
-  popular: () => API.get('movie/popular')
+  popular: () => API.get('movie/popular'),
+  movieDetail: (id: number) =>
+    API.get(`movie/${id}`, {
+      params: {
+        append_to_response: 'videos'
+      }
+    }),
+  search: (term: string) =>
+    API.get('search/movie', {
+      params: {
+        query: encodeURIComponent(term)
+      }
+    })
 };
 
 interface ITvAPI {
   topRated: () => AxiosPromise<any>;
   popular: () => AxiosPromise<any>;
   airingToday: () => AxiosPromise<any>;
+  showDetail: (id: number) => AxiosPromise<any>;
+  search: (string: string) => AxiosPromise<any>;
 }
 
 export const tvAPI: ITvAPI = {
   topRated: () => API.get('tv/top_rated'),
   popular: () => API.get('tv/popular'),
-  airingToday: () => API.get('tv/airing_today')
+  airingToday: () => API.get('tv/airing_today'),
+  showDetail: (id: number) =>
+    API.get(`tv/${id}`, {
+      params: {
+        append_to_response: 'videos'
+      }
+    }),
+  search: (term: string) =>
+    API.get('search/tv', {
+      params: {
+        query: encodeURIComponent(term)
+      }
+    })
 };
