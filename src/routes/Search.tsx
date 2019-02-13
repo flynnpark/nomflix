c9ad8af202a3c65b5802bcdca8f9a2656f245e2b
Flynn <wphestiraid@gmail.com>
2019-02-13 20:34:56 +0900

    검색 API 호출 추가

--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -1,5 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ifError } from 'assert';
+import { MovieItem, TvItem, moviesAPI, tvAPI } from '../api';
 
-const Search: React.FunctionComponent = () => <div>Search</div>;
+const useInput = (defaultValue: string) => {
+  const [value, setValue] = useState(defaultValue);
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value }
+    } = event;
+    console.log(value);
+    setValue(value);
+  };
+
+  return { value, onChange };
+};
+
+const useFetch = (term: string) => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<ErrorEvent | null>(null);
+  const [movieResults, setMovieResult] = useState<MovieItem[] | null>(null);
+  const [results, setResults] = useState<{
+    moviesResults: MovieItem[] | null;
+    tvResults: TvItem[] | null;
+  } | null>(null);
+
+  const fetchData = async () => {
+    try {
+      const {
+        data: { results: moviesResults }
+      } = await moviesAPI.search(term);
+      const {
+        data: { results: tvResults }
+      } = await tvAPI.search(term);
+      setResults({
+        moviesResults,
+        tvResults
+      });
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+};
+
+const Search: React.FunctionComponent = () => {
+  const term = useInput('');
+  return (
+    <div>
+      <input value={term.value} onChange={term.onChange} />
+    </div>
+  );
+};
 
 export default Search;
