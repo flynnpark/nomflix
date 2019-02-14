bc8e428e23c780be3f269aa41bda05b44c41692f
Flynn <wphestiraid@gmail.com>
2019-02-14 20:37:08 +0900

    TV 컴포넌트 수정

--- src/routes/TV.tsx
+++ src/routes/TV.tsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import { TvItem, tvAPI } from '../api';
+import Loading from '../components/Loading';
+import Section from '../components/Section';
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
 
 const useFetch = () => {
   const [loading, setLoading] = useState<boolean>(true);
@@ -42,7 +49,31 @@ const useFetch = () => {
 
 const TV: React.FunctionComponent = () => {
   const { loading, result, error } = useFetch();
-  return <div>TV</div>;
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Container>
+          {result && result.topRated && result.topRated.length > 0 && (
+            <Section title="Now Playing">
+              {result.topRated.map(show => show.name)}
+            </Section>
+          )}
+          {result && result.popular && result.popular.length > 0 && (
+            <Section title="Upcoming">
+              {result.popular.map(show => show.name)}
+            </Section>
+          )}
+          {result && result.airingToday && result.airingToday.length > 0 && (
+            <Section title="Popular">
+              {result.airingToday.map(show => show.name)}
+            </Section>
+          )}
+        </Container>
+      )}
+    </>
+  );
 };
 
 export default TV;
