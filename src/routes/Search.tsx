0b6e62af529492bb9a454c03b9761c16f573f287
Flynn <wphestiraid@gmail.com>
2019-02-16 13:14:39 +0900

    Search 검색 기능 적용
    - useEffect 함수에 대해 더 알아봐야할 듯 하다

--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -1,5 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import { MovieItem, TvItem, moviesAPI, tvAPI } from '../api';
+import Loading from '../components/Loading';
+import Section from '../components/Section';
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Form = styled.form`
+  margin-bottom: 50px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  all: unset;
+  font-size: 28px;
+  width: 100%;
+`;
 
 const useInput = (defaultValue: string) => {
   const [value, setValue] = useState(defaultValue);
@@ -8,7 +26,6 @@ const useInput = (defaultValue: string) => {
     const {
       currentTarget: { value }
     } = event;
-    console.log(value);
     setValue(value);
   };
 
@@ -19,20 +36,20 @@ const useFetch = (term: string) => {
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<ErrorEvent | null>(null);
   const [results, setResults] = useState<{
-    moviesResults: MovieItem[] | null;
+    movieResults: MovieItem[] | null;
     tvResults: TvItem[] | null;
   } | null>(null);
 
   const fetchData = async () => {
     try {
       const {
-        data: { results: moviesResults }
+        data: { results: movieResults }
       } = await moviesAPI.search(term);
       const {
         data: { results: tvResults }
       } = await tvAPI.search(term);
       setResults({
-        moviesResults,
+        movieResults,
         tvResults
       });
     } catch (e) {
@@ -44,17 +61,46 @@ const useFetch = (term: string) => {
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [term]);
 
   return { loading, results, error };
 };
 
 const Search: React.FunctionComponent = () => {
   const term = useInput('');
+  const [termValue, setTermValue] = useState('');
+  const { loading, results, error } = useFetch(termValue);
   return (
-    <div>
-      <input value={term.value} onChange={term.onChange} />
-    </div>
+    <Container>
+      <Form
+        onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+          event.preventDefault();
+          setTermValue(term.value);
+        }}
+      >
+        <Input {...term} placeholder="Search Movies or TV Shows..." />
+      </Form>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          {results && results.movieResults && results.movieResults.length > 0 && (
+            <Section title="Movie Results">
+              {results.movieResults.map(movie => (
+                <span key={movie.id}>{movie.title}</span>
+              ))}
+            </Section>
+          )}
+          {results && results.tvResults && results.tvResults.length > 0 && (
+            <Section title="TV Show Results">
+              {results.tvResults.map(show => (
+                <span key={show.id}>{show.name}</span>
+              ))}
+            </Section>
+          )}
+        </>
+      )}
+    </Container>
   );
 };
 
