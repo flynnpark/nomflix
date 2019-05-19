5e047a674bf15453c7e2ce0662779659b7ad78ce
Flynn <wphestiraid@gmail.com>
2019-05-19 22:39:53 +0900

    Updates

---
+++ precache-manifest.e667204bff5a0d43624940e739b8022e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3ccb4199972e960fec7cc18181f1aa",
+    "url": "/nomflix/index.html"
+  },
+  {
+    "revision": "f775580d9974e6275427",
+    "url": "/nomflix/static/js/2.82d67052.chunk.js"
+  },
+  {
+    "revision": "c7360d39a007cc05d287",
+    "url": "/nomflix/static/js/main.313eb8da.chunk.js"
+  },
+  {
+    "revision": "f646dc5cfa040e8bc513",
+    "url": "/nomflix/static/js/runtime~main.76846126.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/nomflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nomflix/precache-manifest.b5391e3d76238cdb402d9ff76fe089f4.js"
+  "/nomflix/precache-manifest.e667204bff5a0d43624940e739b8022e.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/nomflix/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nomflix/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
